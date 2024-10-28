48e727d3207a505eccde32a3da696e6bf261d570
Mishuman120 <lukasmishustin@gmail.com>
2024-10-28 09:24:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const buyNowMessage = document.querySelector(".bottom-message p");
+    buyNowMessage.addEventListener("click", function() {
+        alert("Thank you for your interest! More information coming soon.");
+    });
+});
